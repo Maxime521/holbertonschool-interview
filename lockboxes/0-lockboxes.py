9cb93ebe081f03b4f1067a5650dbdfd0e08c1d97
Maxime521 <9842@holbertonstudents.com>
2026-02-10 13:26:40 +0100

    Add solution for lockboxes problem

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Utilities for checking if a set of lockboxes can all be opened"""
+
+def canUnlockAll(boxes):
+    """Check if all the boxes can be opened"""
+
+    unlocked = [False] * len(boxes)
+    unlocked[0] = True
+
+    keys = [0]
+    box = keys.pop()
+    for key in boxes[box]:
+        if key < len(boxes) and not unlocked[key]:
+            unlocked[key] = True
+            keys.append(key)
+
+    return all(unlocked)
