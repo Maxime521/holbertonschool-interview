a35900c56e173b33dd118cbe23e82cbf00c28e2d
Maxime521 <9842@holbertonstudents.com>
2026-03-10 12:48:41 +0100

    I just finished the 2 project algo

---
+++ palindrome_integer/palindrome.h
@@ -0,0 +1,21 @@
+#ifndef PALINDROME_H_H
+#define PALINDROME_H_H
+
+#include <stddef.h>
+
+/**
+ * is_palindrome - checks if a number is a palindrome
+ * @n: the number to check
+ *
+ * Return: 1 if the number is a palindrome, 0 otherwise
+ */
+
+int is_palindrome(unsigned long n);
+
+
+
+
+
+
+
+#endif
