d092393c611db762edb4ae03cdeeaddc32364a09
Maxime521 <9842@holbertonstudents.com>
2026-04-10 23:06:44 +0200

    modif the code

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,59 +1,41 @@
 #!/usr/bin/python3
+"""
+Prints all possible N-queens solutions as lists of [row, col] pairs.
+"""
+
 import sys
 
 
-def is_safe(board, row, col, n):
-    for i in range(col):
-        if board[i] == row:
+def is_safe(queens, row, col):
+    """Return True if is safe."""
+    for r, c in queens:
+        if c == col or abs(c - col) == abs(r - row):
             return False
-
-    i = col - 1
-    j = row - 1
-    while i >= 0 and j >= 0:
-        if board[i] == j:
-            return False
-        i -= 1
-        j -= 1
-
-    i = col - 1
-    j = row + 1
-    while i >= 0 and j < n:
-        if board[i] == j:
-            return False
-        i -= 1
-        j += 1
-
     return True
 
 
-def solve_nqueens(board, col, n, solutions):
-    if col >= n:
-        result = []
-        for i in range(n):
-            result.append([i, board[i]])
-        solutions.append(result)
+def solve(n, row, queens, solutions):
+    """place queens row by row."""
+    if row == n:
+        solutions.append([[r, c] for r, c in queens])
         return
 
-    for row in range(n):
-        if is_safe(board, row, col, n):
-            board[col] = row
-            solve_nqueens(board, col + 1, n, solutions)
-            board[col] = -1
+    for col in range(n):
+        if is_safe(queens, row, col):
+            queens.append([row, col])
+            solve(n, row + 1, queens, solutions)
+            queens.pop()
 
 
-def print_solutions(solutions):
-    for solution in solutions:
-        print(solution)
-
-
-if __name__ == "__main__":
+def main():
+    """Solve and print solutions."""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
     try:
         n = int(sys.argv[1])
-    except ValueError:
+    except Exception:
         print("N must be a number")
         sys.exit(1)
 
@@ -61,7 +43,11 @@ if __name__ == "__main__":
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [-1] * n
     solutions = []
-    solve_nqueens(board, 0, n, solutions)
-    print_solutions(solutions)
+    solve(n, 0, [], solutions)
+    for sol in solutions:
+        print(sol)
+
+
+if __name__ == "__main__":
+    main()
